f91a7b93a85da8716233e9c1514037af1d975fd7
abiola <abiola.arobieke@gmail.com>
2024-11-03 14:43:11 -0800

    Add exist, list, and removeDuplicate method

---
+++ javascript/linked_list/linked_list.js
@@ -0,0 +1,95 @@
+class Node {
+  constructor(data) {
+    (this.data = data), (this.next = null);
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {
+    if (this.head === null) {
+      this.head = new Node(data);
+      return;
+    } else {
+      let current = this.head;
+
+      while (current.next != null) {
+        current = current.next;
+      }
+      current.next = new Node(data);
+    }
+  }
+
+  exist(data, current = this.head) {
+    if (current.data == data) {
+      return true;
+    }
+
+    if (current.next == null) {
+      return false;
+    }
+
+    return this.exist(data, (current = current.next));
+  }
+
+  list(current = this.head) {
+    if (current.next == null) {
+      console.log(current.data);
+      return false;
+    }
+
+    console.log(current.data);
+    this.list((current = current.next));
+  }
+
+  removeDuplicate(current = this.head, saved_data = []) {
+    if (current.next == null) {
+      if (saved_data.includes(current.data)) {
+        current.data = null;
+      }
+      return this;
+    }
+
+    if (!saved_data.includes(current.data)) {
+      saved_data.push(current.data);
+    } else {
+      current.data = current.next.data;
+      current.next = current.next.next;
+      return this.removeDuplicate(current, saved_data);
+    }
+
+    return this.removeDuplicate((current = current.next), saved_data);
+  }
+}
+
+num = new LinkedList();
+
+num.add(4);
+num.add(1);
+num.add(1);
+num.add(4);
+num.add(1);
+num.add(1);
+num.add(2);
+num.add(2);
+num.add(4);
+num.add(4);
+num.add(2);
+num.add(5);
+num.add(1);
+num.add(5);
+num.add(2);
+num.add(1);
+num.add(2);
+num.add(5);
+
+// console.log(num.exist(5))
+
+// console.log(num)
+
+console.log(num.removeDuplicate());
+
+num.list();
